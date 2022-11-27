b2c75e6ab6f60d7cfd18e5625a8639d72a19b8b1
lukinoo <luka.bliadze.2015@mail.ru>
2022-11-28 01:50:36 +0400

    adding mp3 convert route logic

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,17 @@ app.get("/", (_req: Request, res: Response): void => {
   });
 });
 
+app.post("/convert-mp3", async (req: Request, res: Response) => {
+  const { mp3Id }: { mp3Id: string } = req.body;
+
+  if (mp3Id === undefined || mp3Id === "".trim() || mp3Id === null) {
+    return res.render("index", {
+      succsess: false,
+      message: "Please Enter youtube video Url...",
+    });
+  }
+
+  console.log(mp3Id);
+});
+
 app.listen(5500);
