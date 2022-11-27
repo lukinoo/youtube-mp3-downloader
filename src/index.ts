0895e4e38e0a119f22c4215cb312587e95045321
lukinoo <luka.bliadze.2015@mail.ru>
2022-11-27 22:14:09 +0400

    creating Scss styles and configurations🔧

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,13 @@ app.use(cors());
 
 app.set("view engine", "ejs");
 
+// scss middleware
+app.use(express.static("public"));
+
 app.get("/", (_req: Request, res: Response): void => {
-  res.render("index.ejs", { name: "world" });
+  res.render("index.ejs", {
+    title: "MP3 downloader",
+  });
 });
 
 app.listen(5500);
